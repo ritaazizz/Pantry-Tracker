9471a8f8bb6fe61f3441dec0310693a48f9e08d6
Rita Aziz <ritaaziz@ritas-mac.mynetworksettings.com>
2024-08-06 18:38:20 -0400

    updated

--- firebase.js
+++ firebase.js
@@ -8,13 +8,12 @@ import { getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrIHYbFCuQOToqF5FmB-6L4lpRxccBca0",
-  authDomain: "inventory-management-ad94c.firebaseapp.com",
-  projectId: "inventory-management-ad94c",
-  storageBucket: "inventory-management-ad94c.appspot.com",
-  messagingSenderId: "172376327301",
-  appId: "1:172376327301:web:31fd0e6caf41744afc95ec",
-  measurementId: "G-CWWGXKKZ1T"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
